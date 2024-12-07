f6bffe3a4f9aa48cf17b9fba2008bf00265afef1
Lim <calmlism@163.com>
2024-12-07 15:08:56 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,554 @@
+
+var ContractAddress = {
+	presale: "0x371e3CD3F2276f5E6e59B46a8b10bc3DF75Bb9c8",
+};
+
+var chainIDList = [11155111];
+
+var chainList = {
+	11155111: {
+		presale: "0x371e3CD3F2276f5E6e59B46a8b10bc3DF75Bb9c8"
+	}
+};
+
+
+var Presale_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_dev",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_sender",
+				"type": "address"
+			}
+		],
+		"name": "AddLiquidity",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_sender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "time",
+				"type": "uint256"
+			}
+		],
+		"name": "Buy",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_sender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "time",
+				"type": "uint256"
+			}
+		],
+		"name": "Claim",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "CLAIM_STATE",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "CLAIM_TOKEN_TOTAL",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "FIRST_PRIZE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "LAST_PRIZE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "LP_ETH_AMOUNT",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "LP_TOKEN_AMOUNT",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "LUCKY_INTERVAL",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "PRESALE_LIMIT",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "SECOND_PRIZE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "SINGLE_FEE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "THIRD_PRIZE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "WithdrawETH",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "WithdrawToken",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "addLPToken",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"name": "buy",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "buyAddressCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "claim",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "firstPrizeAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_index",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_size",
+				"type": "uint256"
+			}
+		],
+		"name": "getPageBuyDesc",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "indexArray",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_index",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_size",
+				"type": "uint256"
+			}
+		],
+		"name": "getPageLuckyDesc",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "indexArray",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "getTokenAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastPrizeAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "luckyAddressCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_sender",
+				"type": "address"
+			}
+		],
+		"name": "queryUser",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "balances",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "lastTime",
+						"type": "uint256"
+					},
+					{
+						"internalType": "bool",
+						"name": "claim",
+						"type": "bool"
+					},
+					{
+						"internalType": "bool",
+						"name": "state",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct CBomberPresale.User",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "secondPrizeAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "thirdPrizeAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "tokenAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "total",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
---
+++ js/extensions.js
@@ -0,0 +1,217 @@
+var web3;
+
+async function IsMetamaskAvailable() {
+    if (window.ethereum) return true;
+    return false;
+}
+
+async function EnableEthereum() {
+    // const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    // ethereum.autoRefreshOnNetworkChange = false;
+    await ethereum.enable();
+
+}
+
+async function GetSelectedAddress() {
+    return await ethereum.selectedAddress;
+}
+
+async function GetChainId() {
+    const chainId = await ethereum.request({ method: 'eth_chainId' });
+    return chainId;
+}
+
+
+async function SwitchChainID(chainID, chainName, chainRpcUrls, blockExplorerUrls, coinName, coinSymbol, coinDecimals) {
+
+    try {
+        await ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: chainID }],
+        });
+
+        return true;
+    } catch (switchError) {
+        // This error code indicates that the chain has not been added to MetaMask.
+        if (switchError.code === 4902) {
+            try {
+                await ethereum.request({
+                    method: 'wallet_addEthereumChain',
+                    params: [
+                        {
+                            chainId: chainID,
+                            chainName: chainName,
+                            rpcUrls: [chainRpcUrls],
+                            blockExplorerUrls: [blockExplorerUrls],
+                            nativeCurrency: {
+                                name: coinName,
+                                symbol: coinSymbol,
+                                decimals: parseInt(coinDecimals)
+                            }
+                        },
+                    ],
+                });
+                return true;
+            } catch (addError) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
+
+
+async function getPresaleLimit() {
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var nums = await presaleContract.methods.PRESALE_LIMIT().call();
+    return web3.utils.fromWei(nums);
+}
+
+async function getTotal() {
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var nums = await presaleContract.methods.total().call();
+    return web3.utils.fromWei(nums);
+}
+
+async function getTokenTotal() {
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var nums = await presaleContract.methods.CLAIM_TOKEN_TOTAL().call();
+    return web3.utils.fromWei(nums);
+}
+
+async function queryUserInfo() {
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var address = await GetSelectedAddress();
+    var info = await presaleContract.methods.queryUser(address).call();
+    return info;
+}
+
+async function getTokenAmount() {
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var address = await GetSelectedAddress();
+    var nums = await presaleContract.methods.getTokenAmount(address).call();
+    return web3.utils.fromWei(nums);
+}
+
+async function getPageBuyDesc(_top){
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var buyCount = await presaleContract.methods.buyAddressCount().call();
+    var infoArray = await presaleContract.methods.getPageBuyDesc(buyCount,_top).call();
+    return infoArray;
+}
+
+async function getPageLuckyDesc(_top){
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var luckyCount = await presaleContract.methods.luckyAddressCount().call();
+    var infoArray = await presaleContract.methods.getPageLuckyDesc(luckyCount,_top).call();
+    return infoArray;
+}
+
+async function getFirstPrizeAddress(){
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    
+    var info = await presaleContract.methods.firstPrizeAddress().call();
+    return info;
+}
+
+async function getSecondPrizeAddress(){
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    
+    var info = await presaleContract.methods.secondPrizeAddress().call();
+    return info;
+}
+
+async function getThirdPrizeAddress(){
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    
+    var info = await presaleContract.methods.thirdPrizeAddress().call();
+    return info;
+}
+
+async function getLastPrizeAddress(){
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    
+    var info = await presaleContract.methods.lastPrizeAddress().call();
+    return info;
+}
+
+async function buyToken(_number){
+
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    var fee = await presaleContract.methods.SINGLE_FEE().call();
+    var feeAll = fee * _number;
+    var address = await GetSelectedAddress();
+    var _data = presaleContract.methods.buy(_number).encodeABI();
+
+    var transactionParameters = {
+        to: ContractAddress.presale,
+        from: address,
+        value: feeAll,
+        data: _data
+    };
+
+    var _gasPrice = await web3.eth.getGasPrice();
+    var _gasLimit;
+    try {
+        _gasLimit = await web3.eth.estimateGas(transactionParameters);
+    } catch (e) {
+        var msgStr = e.message.substring(25);
+        var msg = JSON.parse(msgStr);
+
+        return {
+            state: false,
+            msg: msg.message
+        };
+    }
+    transactionParameters.gasLimit = web3.utils.toHex(_gasLimit);
+    transactionParameters.gasPrice = web3.utils.toHex(_gasPrice);
+
+    var hash = await ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+    });
+
+    return {
+        state: true,
+        msg: hash
+    };
+}
+
+
+async function claimToken(){
+
+    var presaleContract = new web3.eth.Contract(Presale_ABI, ContractAddress.presale);
+    
+    var address = await GetSelectedAddress();
+    var _data = presaleContract.methods.claim().encodeABI();
+
+    var transactionParameters = {
+        to: ContractAddress.presale,
+        from: address,
+        value: "0x00",
+        data: _data
+    };
+
+    var _gasPrice = await web3.eth.getGasPrice();
+    var _gasLimit;
+    try {
+        _gasLimit = await web3.eth.estimateGas(transactionParameters);
+    } catch (e) {
+        return {
+            state: false,
+            msg: e.message
+        };
+    }
+    transactionParameters.gasLimit = web3.utils.toHex(_gasLimit);
+    transactionParameters.gasPrice = web3.utils.toHex(_gasPrice);
+
+    var hash = await ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+    });
+
+    return {
+        state: true,
+        msg: hash
+    };
+}
